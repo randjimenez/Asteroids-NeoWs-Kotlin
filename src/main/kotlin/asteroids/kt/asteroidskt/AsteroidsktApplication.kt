5a319dee3914d3bb6e8705781518a4cdd1eae865
Rafael Jimenez <ext_randjimenezm@falabella.cl>
2023-02-01 14:15:07 -0500

    Kotlin base code

---
+++ src/main/kotlin/asteroids/kt/asteroidskt/AsteroidsktApplication.kt
@@ -0,0 +1,11 @@
+package asteroids.kt.asteroidskt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsteroidsktApplication
+
+fun main(args: Array<String>) {
+    runApplication<AsteroidsktApplication>(*args)
+}
---
+++ src/test/kotlin/asteroids/kt/asteroidskt/AsteroidsktApplicationTests.kt
@@ -0,0 +1,13 @@
+package asteroids.kt.asteroidskt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsteroidsktApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
